64dfb1912cda448756098d8de71bae5909f599e8
marcelin monso <marcelin.tagba@gmail.com>
2022-08-10 07:48:02 +0000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <SideBarApp :menus="menus" :showmenu="showmenu" :isOpen="isOpen" :showSide="showSide" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import Vue from 'vue';
+import SideBarApp from "mairie-sidebar-app";
 
-@Component({
+export default Vue.extend({
+  name: 'App',
   components: {
-    HelloWorld,
+    SideBarApp
   },
-})
-export default class App extends Vue {}
+   data() {
+    return {
+      menus: [
+        {
+          link: "/settingsOrchestre",
+          titleMenu: "SettingsOrchestre",
+          menuIcon: "face",
+        },
+        {
+          link: "/featuresOrchestre",
+          titleMenu: "Features",
+          menuIcon: "streetview",
+          subMenus: [
+            {
+              title: "PagesOrchestre",
+              link: "/pagesOrchestre",
+              icon: "streetview",
+            },
+            {
+              title: "ElementsOrchestre",
+              link: "/elementsOrchestre",
+              icon: "streetview",
+            },
+          ],
+        },
+        {
+          link: "/ServicesOrchestre",
+          titleMenu: "Services",
+          menuIcon: "face",
+          subMenus: [
+            {
+              title: "App Design Orchestre",
+              link: "/app-designOrchestre",
+              icon: "account_circle",
+            },
+            {
+              title: "Web Design Orchestre",
+              link: "/web-designOrchestre",
+              icon: "work",
+            },
+          ],
+        },
+        {
+          link: "/OverviewOrchestre",
+          titleMenu: "OverviewOrchestre",
+          menuIcon: "settings",
+        }
+      ],
+      showmenu: false,
+      isOpen: false,
+      showSide: true, 
+    }
+  },
+});
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "app.scss";
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+import '../node_modules/mairie-sidebar-app/dist/SideBarApp.css'
+
+Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin) 
+
+Vue.config.productionTip = false
 
 Vue.config.productionTip = false
 
